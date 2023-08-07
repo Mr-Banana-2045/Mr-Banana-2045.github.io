21479016077c7ef20a1d06e5348e0ef5a44b539c
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2023-08-07 12:52:34 +0330

    Update data.php

--- data.php
+++ data.php
@@ -1,43 +1,8 @@
 <?php
-	
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	function get_data() {
-			 
-      $file_name='testing'. '.json';
-
-	if(file_exists("$file_name")) {
-		$current_data=file_get_contents("$file_name");
-		$array_data=json_decode($current_data, true);
-	$extra=array(
-	
-			'id' => $_POST['id'],
-		
+$message = $_POST['id'];
+$data = array(
+'id' => $message
 );
-		$array_data[]=$extra;
-		echo "true";
-		return json_encode($array_data);
-}
-else {
-	$datae=array();
-	$datae[]=array(
-	
-			'id' => $_POST['id'],
-			
-);
-echo "no file";
-	return json_encode($datae);
-}
-}
-
-	$file_name='testing'. '.json';
-	
-	if(file_put_contents("$file_name", get_data())) {
-		echo 'true';
-	}				
-	else {
-		echo 'error';				
-	}
-}
-	
+$json_data = json_encode($data);
+file_put_contents('testing.json', $json_data);
 ?>
-	
