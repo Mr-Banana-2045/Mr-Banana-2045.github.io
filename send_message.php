64bea194add335d3924fb32e6b496eebc13fe325
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2023-08-16 17:59:48 +0330

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -3,11 +3,9 @@
     if (isset($_POST["message"])) {
         $message = $_POST["message"];
         $username = "User"; // نام کاربر را می‌توانید با استفاده از متغیرهای دیگر تعیین کنید
-        $data = file_get_contents("messages.json");
-        $messages = json_decode($data, true);
-        $newMessage = array("username" => $username, "message" => $message);
-        $messages[] = $newMessage;
-        file_put_contents("messages.json", json_encode($messages));
+        $file = fopen("messages.txt", "a");
+        fwrite($file, $username . ": " . $message . "\n");
+        fclose($file);
         echo "Message sent successfully!";
     }
 ?>
