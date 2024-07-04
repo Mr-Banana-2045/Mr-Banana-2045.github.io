b308e3f1f28418d9a59fbf9a49c7de7fca8bf744
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2024-07-04 13:57:00 +0330

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,1 +1,3 @@
-echo "moz"
+cmd.exe /c calc.exe
+firstString="CMD.EXE was started with the above path as the current directory. UNC paths are not supported.  Defaulting to Windows directory."
+echo "$firstString" | awk '{gsub("CMD.EXE was started with the above path as the current directory. UNC paths are not supported.  Defaulting to Windows directory.","hello"); print}'
