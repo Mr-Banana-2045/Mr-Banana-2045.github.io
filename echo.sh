af613d194d34cbbb4a9fa3a78d04dd1548ff3ff7
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2024-07-04 13:58:11 +0330

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,3 +1,1 @@
 cmd.exe /c calc.exe
-firstString="CMD.EXE was started with the above path as the current directory. UNC paths are not supported.  Defaulting to Windows directory."
-echo "$firstString" | awk '{gsub("CMD.EXE was started with the above path as the current directory. UNC paths are not supported.  Defaulting to Windows directory.","hello"); print}'
